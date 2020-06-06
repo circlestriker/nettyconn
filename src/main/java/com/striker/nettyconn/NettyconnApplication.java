f73380fffbd37cf881d309cdac7cc7b44b86c584
win <69039958@qq.com>
2020-06-06 13:45:35 +0800

    boot

---
+++ src/main/java/com/striker/nettyconn/NettyconnApplication.java
@@ -0,0 +1,13 @@
+package com.striker.nettyconn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyconnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NettyconnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/striker/nettyconn/NettyconnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.striker.nettyconn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyconnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
